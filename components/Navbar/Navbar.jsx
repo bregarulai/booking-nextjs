6407d1bb276fbc35663ef1c65beafcd9e372997d
bregarulai <bregarulai@gmail.com>
2022-05-06 22:24:58 -0400

    created navbar component

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='navbar__container'>
+        <sppan className='navbar__logo'>BregaCode Booking</sppan>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
