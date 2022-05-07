136a960b3b825b6bdf1b69214f57c5e3fbdeb0a5
bregarulai <bregarulai@gmail.com>
2022-05-06 22:01:03 -0400

    first commit

--- components/hello-world.js
+++ components/hello-world.js
@@ -1,9 +1,7 @@
-import styles from './hello-world.module.scss'
-
 export default function HelloWorld() {
   return (
-    <div className={styles.hello}>
-      Hello World, I am being styled using SCSS Modules!
+    <div className='hello'>
+      Hello World, I am being styled using SCSS Modules nuevo nuevo!
     </div>
-  )
+  );
 }
