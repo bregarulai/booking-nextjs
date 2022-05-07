28d39a3d771e1a74aa7f244c8c42d920e752f6e2
bregarulai <bregarulai@gmail.com>
2022-05-07 15:14:10 -0400

    created jsx for mailList component

--- components/Maillist/MailList.jsx
+++ components/Maillist/MailList.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const MailList = () => {
-  return <div>MailList</div>;
+  return (
+    <div className='mailList'>
+      <h1 className='mailList__title'>Save time, save money!</h1>
+      <span className='mailList__description'>
+        Sign up and we'll send the best deals to you
+      </span>
+      <div className='mailList__input-container'>
+        <input type='email' placeholder='Your Email' />
+        <button>Subscribe</button>
+      </div>
+    </div>
+  );
 };
 
 export default MailList;
