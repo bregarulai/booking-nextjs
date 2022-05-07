a53e23d7b7384de6f7731e8c9e109d9d650a9e8e
bregarulai <bregarulai@gmail.com>
2022-05-07 15:41:03 -0400

    created footer jsx

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,7 +1,55 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className='footer'>
+      <div className='footer__lists'>
+        <ul className='footer__list'>
+          <li className='footer__list-item'>Countries</li>
+          <li className='footer__list-item'>Regions</li>
+          <li className='footer__list-item'>Cities</li>
+          <li className='footer__list-item'>Districts</li>
+          <li className='footer__list-item'>Airports</li>
+          <li className='footer__list-item'>Hotels</li>
+        </ul>
+        <ul className='footer__list'>
+          <li className='footer__list-item'>Home</li>
+          <li className='footer__list-item'>Apartments</li>
+          <li className='footer__list-item'>Cities</li>
+          <li className='footer__list-item'>Resolrts</li>
+          <li className='footer__list-item'>Villas</li>
+          <li className='footer__list-item'>Guest houses</li>
+        </ul>
+        <ul className='footer__list'>
+          <li className='footer__list-item'>Unique places to stay</li>
+          <li className='footer__list-item'>All destinations</li>
+          <li className='footer__list-item'>Discover</li>
+          <li className='footer__list-item'>Reviews</li>
+          <li className='footer__list-item'>Travel Communities</li>
+          <li className='footer__list-item'>Seasonal and holiday Deals</li>
+        </ul>
+        <ul className='footer__list'>
+          <li className='footer__list-item'>Car rental</li>
+          <li className='footer__list-item'>Flight finder</li>
+          <li className='footer__list-item'>Restaurant reservations</li>
+          <li className='footer__list-item'>Booking for Travel Agents</li>
+        </ul>
+        <ul className='footer__list'>
+          <li className='footer__list-item'>Coronavirus (COVID-19) FAQs</li>
+          <li className='footer__list-item'>About us</li>
+          <li className='footer__list-item'>Customer Service Help</li>
+          <li className='footer__list-item'>Partner help</li>
+          <li className='footer__list-item'>Careers</li>
+          <li className='footer__list-item'>Sustainability</li>
+          <li className='footer__list-item'>Press center</li>
+          <li className='footer__list-item'>Safty Resource Center</li>
+          <li className='footer__list-item'>Investor relations</li>
+          <li className='footer__list-item'>Terms and conditions</li>
+        </ul>
+      </div>
+      <div className='footer__text'>Copyright © 2022 BregaBooking.</div>
+    </div>
+  );
 };
 
 export default Footer;
--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <div className='navbar__container'>
-        <sppan className='navbar__logo'>BregaCode Booking</sppan>
+        <sppan className='navbar__logo'>BregaBooking</sppan>
         <div className='navbar__items'>
           <button className='navbar__button'>Register</button>
           <button className='navbar__button'>Login</button>
