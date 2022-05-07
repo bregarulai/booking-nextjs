da76439eaa969f1a473c74d9908eb6a6d5fba2db
bregarulai <bregarulai@gmail.com>
2022-05-06 22:37:55 -0400

    implemented navbar component

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -5,6 +5,10 @@ const Navbar = () => {
     <div className='navbar'>
       <div className='navbar__container'>
         <sppan className='navbar__logo'>BregaCode Booking</sppan>
+        <div className='navbar__items'>
+          <button className='navbar__button'>Register</button>
+          <button className='navbar__button'>Login</button>
+        </div>
       </div>
     </div>
   );
