c9cfb4a03890494059116bfa8581805775ef2e94
bregarulai <bregarulai@gmail.com>
2022-05-12 18:56:10 -0400

    added link to home page in navbar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <div className='navbar__container'>
-        <span className='navbar__logo'>BregaBooking</span>
+        <Link href='/'>
+          <span className='navbar__logo'>BregaBooking</span>
+        </Link>
         <div className='navbar__items'>
           <button className='navbar__button'>Register</button>
           <button className='navbar__button'>Login</button>
